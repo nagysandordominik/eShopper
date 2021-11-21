e2ad0eb49111cee2d0a1567ef432f72fc87ef8a5
nagysandordominik <nagysandordominik@icloud.com>
2021-11-21 20:20:04 +0100

    express server setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi there')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) =>{
+    res.send('hello there!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening');
+});
