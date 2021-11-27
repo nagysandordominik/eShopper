9507b67752488a986513c37afc702d8839af2db0
nagysandordominik <nagysandordominik@icloud.com>
2021-11-27 13:06:05 +0100

    modifying existing users data with update function

--- repos/users.js
+++ repos/users.js
@@ -37,9 +37,11 @@ class UsersRepository {
             JSON.stringify(records,null,2) 
         );
     }
+
     randomId() {
         return crypto.randomBytes(4).toString('hex');
     }
+
     async getOne(id) {
         const records = await this.getAll();
         return records.find(record => record.id === id);
@@ -50,12 +52,23 @@ class UsersRepository {
         const filteredRecords = records.filter(record => record.id !== id);
         await this.writeAll(filteredRecords);
     }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error (`Record with id ${id} not found`)
+        }
+        Object.assign(record, attrs);
+        await this.writeAll(records);
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    await repo.delete('288dcbc0')
+    await repo.update('57423d2d', {password: 'asdasd'});
 };
 
 test();
