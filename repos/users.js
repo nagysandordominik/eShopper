1af580c1c5cd3b0034a517462991df892e51ed2a
nagysandordominik <nagysandordominik@icloud.com>
2021-11-28 19:36:06 +0100

    finding user in db by one data

--- repos/users.js
+++ repos/users.js
@@ -63,12 +63,33 @@ class UsersRepository {
         Object.assign(record, attrs);
         await this.writeAll(records);
     }
+
+    async getOneBy(filters) {
+        const records = await this.getAll();
+        for (let record of records) {
+            let found = true;
+
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false;
+                }
+            }
+            
+            if (found) {
+                return record;
+            } else {
+                throw new Error ('User not found...')
+            }
+        }
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    await repo.update('57423d2d', {password: 'asdasd'});
-};
+    const user = await repo.getOneBy( {email: 'asd@test.hu'});
 
+    console.log(user);
+};
+ 
 test();
