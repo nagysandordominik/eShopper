aa0a0e239064774d9ffae9191db9d9ad91fa414b
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-02 20:05:08 +0100

    password hash

--- repos/users.js
+++ repos/users.js
@@ -3,7 +3,9 @@ const { json } = require('express');
 const fs = require('fs');
 const crypto = require('crypto');
 const { get } = require('http');
+const util = require('util');
 
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
     constructor(filename){
@@ -26,8 +28,16 @@ class UsersRepository {
 }
     async create(attrs) {
         attrs.id = this.randomId();
+
+        const salt = crypto.randomBytes(8).toString('hex');
+        const buf = await scrypt(attrs.password,salt,64);
+
         const records = await this.getAll();
-        records.push(attrs);
+        const record = {
+            ...attrs,
+            password:`${buf.toString('hex')}.${salt}`
+        };
+        records.push(record);
 
         await this.writeAll(records);
 
