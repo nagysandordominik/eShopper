a1ed7696df74b8f4604eda271eeb67103aa90253
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-07 21:11:36 +0100

    custom validation

--- repos/routes/admin/auth.js
+++ repos/routes/admin/auth.js
@@ -16,13 +16,27 @@ router.post('/signup',
     check('email')
         .trim()
         .normalizeEmail()
-        .isEmail(),
+        .isEmail()
+        .withMessage('Must be a valid email')
+        .custom( async (email)=> {
+            const existingUser = await usersRepo.getOneBy({ email });
+            if (existingUser) {
+                throw new Error ('Email already used')
+            }
+        }),
     check('password')
         .trim()
-        .isLength({min: 4, max: 20}),
+        .isLength({min: 4, max: 20})
+        .withMessage('Must be between 4 and 20 characters'),
     check('passwordConfirmation')
         .trim()
         .isLength({min: 4, max: 20})
+        .withMessage('Must be between 4 and 20 characters')
+        .custom((passwordConfirmation, { req }) => {
+            if (passwordConfirmation !== req.body.password) {
+                throw new Error('Passwords must match');
+            }
+        })
     ],  
     async (req,res) => {
     const errors = validationResult(req);
@@ -30,10 +44,7 @@ router.post('/signup',
     
     const { email, password, passwordConfirmation} = req.body;
 
-    const existingUser = await usersRepo.getOneBy({ email });
-    if (existingUser) {
-        return res.send('Email is already used...')
-    }
+    
 
     if (password !== passwordConfirmation) {
         return res.send('Password must match...')
