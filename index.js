4d328a7789205ed4ee82735410db666f544130e7
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-13 19:35:04 +0100

    route created for products.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const usersRepo = require('./repos/users');
 const cookieSession = require('cookie-session');
-const authRouter = require('./routes/admin/auth')
+const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
     keys: ['asdaskg2fa2d244caws']
 }));
 app.use(authRouter);
+app.use(productsRouter);
 
 
 app.listen(3000, () => {
