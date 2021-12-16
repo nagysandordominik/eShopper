2e4419804b5ce1c6cc3e50d08495b89486026cfe
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-16 20:55:34 +0100

    add: post request handler for new products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const {validationResult} = require('express-validator');
 const productRepo = require('../../repos/products');
+const productsNewTemp = require('../../views/admin/products/new')
+const { requireTitle, requirePrice } = require('./validators');
+
 
 const router = express.Router();
 
@@ -8,7 +12,15 @@ router.get('/admin/products',(req,res) => {
 });
 
 router.get('/admin/products/new',(req,res) => {
-    
+    res.send(productsNewTemp({}));
+});
+
+
+router.post('/admin/products/new', [requireTitle,requirePrice], (req,res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+
+    res.send('submitted');
 });
 
 module.exports = router;
