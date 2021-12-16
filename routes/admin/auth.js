a8c831da9313ac66a79e2c73055153d0a05aa633
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-16 20:51:14 +0100

    add: product input required

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -36,13 +36,16 @@ router.get('/signout', (req,res) =>{
 });
 
 router.get('/signin', (req,res) =>{
-    res.send(signinTemp({req}));
+    res.send(signinTemp({}));
 });
 
 router.post('/signin',[requireEmailExists, requireValidPassword], 
     async (req,res) => {
     const errors = validationResult(req);
-    console.log(errors);
+    
+    if (!errors.isEmpty()) {
+        return res.send(signinTemp({errors}));
+    }
 
     const { email } = req.body;
 
