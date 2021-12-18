cb6d1596a8457a668ce12d392bc0090454a6d567
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-18 20:35:34 +0100

    add: multer npm for image upload handling

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,9 +3,10 @@ const {validationResult} = require('express-validator');
 const productRepo = require('../../repos/products');
 const productsNewTemp = require('../../views/admin/products/new')
 const { requireTitle, requirePrice } = require('./validators');
-
+const multer = require('multer');
 
 const router = express.Router();
+const upload = multer({ storage:multer.memoryStorage() });
 
 router.get('/admin/products',(req,res) => {
 
@@ -16,8 +17,10 @@ router.get('/admin/products/new',(req,res) => {
 });
 
 
-router.post('/admin/products/new', [requireTitle,requirePrice], (req,res) => {
+router.post('/admin/products/new', [requireTitle,requirePrice], upload.single('image'), (req,res) => {
     const errors = validationResult(req);
+
+    console.log(req.file); 
 
     req.on('data' , data => {
         console.log(data.toString());
