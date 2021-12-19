b56d529d5aedceb05b962333257863e49c7e46eb
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-19 17:56:18 +0100

    add: image saving to json file

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -17,16 +17,22 @@ router.get('/admin/products/new',(req,res) => {
 });
 
 
-router.post('/admin/products/new', [requireTitle,requirePrice], upload.single('image'), (req,res) => {
-    const errors = validationResult(req);
+router.post('/admin/products/new',
+    upload.single('image'),    
+    [requireTitle,requirePrice],  
+    
+    async (req,res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.send(productsNewTemp({errors}))
+        }
 
-    console.log(req.file); 
-
-    req.on('data' , data => {
-        console.log(data.toString());
-    });
+        console.log(req.file.buffer.toString('base64'));
+        const {title, price} = req.body;
+    await productRepo.create({title,price,image}); 
 
     res.send('submitted');
-});
+    }
+);
 
 module.exports = router;
