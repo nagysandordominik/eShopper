4b5ad17819bc98d8014d327e71d51734d1f51449
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-21 21:34:52 +0100

    linked with products-index.js

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,35 +1,33 @@
 const express = require('express');
-const {validationResult} = require('express-validator');
+
+const { handleErrors } = require('./middlewares')
 const productRepo = require('../../repos/products');
 const productsNewTemp = require('../../views/admin/products/new')
+const productsIndexTemp = require('../../views/admin/products/index')
 const { requireTitle, requirePrice } = require('./validators');
 const multer = require('multer');
+const { handleErrors } = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({ storage:multer.memoryStorage() });
 
 router.get('/admin/products',(req,res) => {
-
+    const products = await productRepo.getAll();
+    res.send(productsIndexTemp({products}))
 });
 
 router.get('/admin/products/new',(req,res) => {
     res.send(productsNewTemp({}));
 });
 
-
 router.post('/admin/products/new',
     upload.single('image'),    
-    [requireTitle,requirePrice],  
-    
+    [requireTitle,requirePrice],
+    handleErrors(productsNewTemp),  
     async (req,res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.send(productsNewTemp({errors}))
-        }
-
         console.log(req.file.buffer.toString('base64'));
         const {title, price} = req.body;
-    await productRepo.create({title,price,image}); 
+        await productRepo.create({title,price,image}); 
 
     res.send('submitted');
     }
