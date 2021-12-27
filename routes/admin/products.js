d2bd74233d69d40a1ff155ee9366c4b1386abb12
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-27 18:37:34 +0100

    add: authentication linked with middlewares.js and route handler

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,21 +5,22 @@ const productsNewTemp = require('../../views/admin/products/new')
 const productsIndexTemp = require('../../views/admin/products/index')
 const { requireTitle, requirePrice } = require('./validators');
 const multer = require('multer');
-const { handleErrors } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({ storage:multer.memoryStorage() });
 
-router.get('/admin/products', async(req,res) => {
+router.get('/admin/products',requireAuth, async(req,res) => {
     const products = await productRepo.getAll();
     res.send(productsIndexTemp({products}))
 });
 
-router.get('/admin/products/new',(req,res) => {
+router.get('/admin/products/new',requireAuth,(req,res) => {
     res.send(productsNewTemp({}));
 });
 
 router.post('/admin/products/new',
+    requireAuth,
     upload.single('image'),    
     [requireTitle,requirePrice],
     handleErrors(productsNewTemp),  
