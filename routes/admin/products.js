3a30c3140bb7374c0f007ecaf839058d22f5b7bc
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-28 19:13:40 +0100

    add: url param by product id

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,9 +3,11 @@ const express = require('express');
 const productRepo = require('../../repos/products');
 const productsNewTemp = require('../../views/admin/products/new')
 const productsIndexTemp = require('../../views/admin/products/index')
+const productsEditTemp = require('../../views/admin/products/edit')
 const { requireTitle, requirePrice } = require('./validators');
 const multer = require('multer');
 const { handleErrors, requireAuth } = require('./middlewares');
+const { route } = require('express/lib/application');
 
 const router = express.Router();
 const upload = multer({ storage:multer.memoryStorage() });
@@ -32,5 +34,15 @@ router.post('/admin/products/new',
     res.redirect('/admin/products');
     }
 );
+
+router.get('/admin/products/:id/edit', async (req, res) => {
+    const product = await productRepo.getOne(req.params.id);
+    
+    if (!product) {
+        return res.send('Product not found!');
+    }
+
+    res.send(productsEditTemp({ product }));
+}); 
 
 module.exports = router;
