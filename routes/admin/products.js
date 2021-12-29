fa21c0e80ae0d0c5882fe1aac67d7825e5e30e82
Nagy Sándor Dominik <thesatyka@gmail.com>
2021-12-29 20:37:51 +0100

    add: editing option with updating the data

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,19 +1,19 @@
 const express = require('express');
 
-const productRepo = require('../../repos/products');
+const productsRepo = require('../../repos/products');
 const productsNewTemp = require('../../views/admin/products/new')
 const productsIndexTemp = require('../../views/admin/products/index')
 const productsEditTemp = require('../../views/admin/products/edit')
 const { requireTitle, requirePrice } = require('./validators');
 const multer = require('multer');
 const { handleErrors, requireAuth } = require('./middlewares');
-const { route } = require('express/lib/application');
+
 
 const router = express.Router();
 const upload = multer({ storage:multer.memoryStorage() });
 
 router.get('/admin/products',requireAuth, async(req,res) => {
-    const products = await productRepo.getAll();
+    const products = await productsRepo.getAll();
     res.send(productsIndexTemp({products}))
 });
 
@@ -29,14 +29,14 @@ router.post('/admin/products/new',
     async (req,res) => {
         const image = req.file.buffer.toString('base64');
         const {title, price} = req.body;
-        await productRepo.create({title,price,image}); 
+        await productsRepo.create({title,price,image}); 
 
     res.redirect('/admin/products');
     }
 );
 
 router.get('/admin/products/:id/edit', async (req, res) => {
-    const product = await productRepo.getOne(req.params.id);
+    const product = await productsRepo.getOne(req.params.id);
     
     if (!product) {
         return res.send('Product not found!');
@@ -44,5 +44,27 @@ router.get('/admin/products/:id/edit', async (req, res) => {
 
     res.send(productsEditTemp({ product }));
 }); 
+
+router.post('/admin/products/:id/edit',
+    requireAuth,
+    upload.single('image'),
+    [requireTitle,requirePrice],
+    handleErrors(productsEditTemp),
+    async (req,res) =>  {
+        const changes = req.body;
+
+        if (req.file) {
+            changes.image = req.file.buffer.toString('base64');
+        }
+
+        try {
+            await productsRepo.update(req.params.id, changes)
+        }   catch (err) {
+            return res.send('Could not find item!');
+        }
+
+        res.redirect('/admin/products');    
+    }
+);
 
 module.exports = router;
