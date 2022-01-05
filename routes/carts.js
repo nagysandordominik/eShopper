05a9d63f79c146ebb29f111bd0878ecdbca9f99c
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-01-05 20:19:51 +0100

    add: cart stores product in array by id

--- routes/carts.js
+++ routes/carts.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const cartsRepo = require('../repos/carts');
 const router = express.Router();
 
-router.post('/cart/products', (req,res) => {
-    console.log(req.body.productId)
-    res.send('Product added to cart!')
+router.post('/cart/products', async (req,res) => {
+    let cart;
+   if (!req.session.cartId) {
+        cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+   } else { 
+        cart = await cartsRepo.getOne(req.session.cartId);
+   }
+   console.log(cart);
 });
 
 module.exports = router;
